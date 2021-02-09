277ee209d0613ff2dca28b78c4a73c6ff961604c
Zerobandwidth <nimdavirus@gmail.com>
2021-02-09 16:29:48 +0100

    Create build_dedicated_valheim_server.sh

---
+++ build_dedicated_valheim_server.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+# All in one Script to install Valheim Dedicated Server
+# Change PASSWORDS and CHANGE ME AREAS for launching
+
+#switch to root if not already
+sudo -s
+
+#update system
+apt update && apt upgrade
+
+#add multiverse repo
+add-apt-repository multiverse
+
+#add i386 architecture
+dpkg --add-architecture i386
+
+#update system again
+apt update
+
+#install steamcmd
+apt install steamcmd
+
+#build account to run Valheim CHANGE PASSWORD PLEASE
+useradd --create-home --shell /bin/bash --password CHANGEME steam
+
+# switch to newly created steam user
+su - steam
+
+#build symbolic link for steamcmd
+ln -s /usr/games/steamcmd steamcmd
+
+#set steamcmd execution 
+steamcmd +login anonymous +force_install_dir /home/steam/valheimserver +app_update 896660 validate +exit
+
+#build config for start_valheim.sh
+
+cat >> /home/steam/valheimserver/start_valheim.sh <<EOF
+#!/bin/bash
+export templdpath=$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
+export SteamAppId=892970
+
+# Tip: Make a local copy of this script to avoid it being overwritten by steam.
+# NOTE: Minimum password length is 5 characters & Password cant be in the server name.
+# NOTE: You need to make sure the ports 2456-2458 is being forwarded to your server through your local router & firewall.
+./valheim_server.x86_64 -name "DISPLAYNAMECHANGEME" -port 2456 -nographics -batchmode -world "CHANGEMEWORLDNAME" -password "CHANGEME PASSWORD" -public 1
+
+export LD_LIBRARY_PATH=$templdpath
+EOF
+
+#build check log script
+
+cat >> /home/steam/check_log.sh <<EOF
+journalctl --unit=valheimserver --reverse
+EOF
+
+#set execute permissions
+chmod +x /home/steam/valheimserver/start_valheim.sh
+chmod +x /home/steam/check_log.sh
+
+#leave steam user bash envoriment and enter back into root
+exit
+
+#build systemctl configurations for execution of processes for Valheim Server
+
+cat >> /etc/systemd/system/valheimserver.service <<EOF
+[Unit]
+Description=Valheim Server
+Wants=network-online.target
+After=syslog.target network.target nss-lookup.target network-online.target
+
+[Service]
+Type=simple
+Restart=on-failure
+RestartSec=5
+StartLimitInterval=60s
+StartLimitBurst=3
+User=steam
+Group=steam
+ExecStartPre=/home/steam/steamcmd +login anonymous +force_install_dir /home/steam/valheimserver +app_update 896660 validate +exit
+ExecStart=/home/steam/valheimserver/start_valheim.sh
+ExecReload=/bin/kill -s HUP $MAINPID
+ExecStop=/bin/kill -s INT $MAINPID
+WorkingDirectory=/home/steam/valheimserver
+LimitNOFILE=100000
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+# Reload daemons
+systemctl daemon-reload
+# Start server
+systemctl start valheimserver
+# Enable server on restarts
+systemctl enable valheimserver
+
+
+echo "System needs to reboot to make changes, please reboot and execute selks-first-time-install?"
