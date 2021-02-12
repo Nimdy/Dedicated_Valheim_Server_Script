994ccbb8cb42d99f89febc475abf84d8cb6086c5
Zerobandwidth <nimdavirus@gmail.com>
2021-02-12 10:59:16 +0100

    Update menu.sh
    Changing the format for multi leveled menu system

--- menu.sh
+++ menu.sh
@@ -3,58 +3,137 @@
 # Open to other commands that should be used... 
 clear
 title="~*~*~*~*Valheim Toolbox Menu*~*~*~*~"
-echo "$title"
-PS3='Select Menu option: '
-options=("Valheim Full Server Install" "Check Valheim Server Status" "Stop Valheim Server" "Backup world files" "Restore world files" "Check for Valheim Server updates" "Quit")
-select opt in "${options[@]}"
-do
-    case $opt in
-        "Valheim Full Server Install")
-            echo "Oh for Loki, I am building it"
-            sudo apt-get install -y git net-tools
-            cd /opt
-            sudo git clone https://github.com/Nimdy/Dedicated_Valheim_Server_Script.git
-            cd Dedicated_Valheim_Server_Script/
-            sudo chmod +x build_dedicated_valheim_server.sh
-            sudo ufw allow 2456:2458/udp
-            sleep 2
-            sudo ufw allow 2456:2458/tcp
-            sleep 2
-            sudo ./build_dedicated_valheim_server.sh
-            ;;
-        "Check Valheim Server Status")
-            echo "Check Valheim Server Status"
-            sudo systemctl status valheimserver.service 
-            ;;
-        "Stop Valheim Server")
-            echo "Stopping Valheim Server"
-            sudo systemctl stop valheimserver.service 
-            ;;
-        "Backup world files")
-            echo "Clearing database logs"
-            echo "not added yet"
-            ;;
-         "Restore world files")
-            echo "Clearing database logs"
-            echo "not added yet"
-            ;;
-         "Check for Valheim Server updates")
-            echo "Checking for updates"
-            echo "not added yet"
-            ;;   
-         "Quit")
-            break
-            ;;
-       *) echo invalid option;;
-    esac
-    counter=1
-    SAVEIFS=$IFS
-    echo "~*~*~*~*Valheim Toolbox Menu*~*~*~*~"
-    IFS=$(echo -en "\n\b")
-    for i in ${options[@]};
-    do
-        echo $counter')' $i
-        let 'counter+=1'
-    done
-    IFS=$SAVEIFS
-done
+
+##
+# System script that checks:
+#   - Memory usage
+#   - CPU load
+#   - Number of TCP/UDP connections 
+#   - Kernel version
+##
+
+##
+# Server Tools:
+#   - List Admins
+#   - Add Admin
+#   - Remove Admin
+
+##
+
+server_name=$(hostname)
+
+function memory_check() {
+    echo ""
+	echo "Memory usage on ${server_name} is: "
+	free -h
+	echo ""
+}
+
+function cpu_check() {
+    echo ""
+	echo "CPU load on ${server_name} is: "
+    echo ""
+	uptime
+    echo ""
+}
+
+function tcp_check() {
+    echo ""
+	echo "TCP connections on ${server_name}: "
+    echo ""
+	cat  /proc/net/tcp | wc -l
+    echo ""
+}
+
+function udp_check() {
+    echo ""
+	echo "UDP connections on ${server_name}: "
+    echo ""
+	cat  /proc/net/udp | wc -l
+    echo ""
+}
+
+function kernel_check() {
+    echo ""
+	echo "Kernel version on ${server_name} is: "
+	echo ""
+	uname -r
+    echo ""
+}
+
+function valheim_server_install() {
+    echo ""
+	echo "Installing Valheim"
+	echo "Oh for Loki, its starting"
+	echo "This will work add code later"
+	echo ""
+}
+
+function valheim_update_check() {
+    echo ""
+	echo "Checking for Valheim Server Updates "
+	echo "This will work add code later"
+	echo ""
+    echo ""
+}
+
+function all_checks() {
+	memory_check
+	cpu_check
+	tcp_check
+	udp_check
+	kernel_check
+}
+
+##
+# Color  Variables
+##
+green='\e[32m'
+blue='\e[34m'
+clear='\e[0m'
+
+##
+# Color Functions
+##
+
+ColorGreen(){
+	echo -ne $green$1$clear
+}
+ColorBlue(){
+	echo -ne $blue$1$clear
+}
+
+menu(){
+echo -ne "
+Server System Information
+$(ColorGreen '1)') Memory usage
+$(ColorGreen '2)') CPU load
+$(ColorGreen '3)') Number of TCP connections 
+$(ColorGreen '4)') Number of UDP connections 
+$(ColorGreen '5)') Kernel version
+$(ColorGreen '6)') Check All
+Valheim Server Commands
+$(ColorGreen '7)') Install Valheim Server
+$(ColorGreen '8)') Valheim Server Update Check
+
+
+
+$(ColorGreen '0)') Exit
+$(ColorBlue 'Choose an option:') "
+        read a
+        case $a in
+	        1) memory_check ; menu ;;
+	        2) cpu_check ; menu ;;
+	        3) tcp_check ; menu ;;
+	        4) udp_check ; menu ;;
+	        5) kernel_check ; menu ;;
+	        6) all_checks ; menu ;;
+		    7) valheim_server_install ; menu ;;
+	    	8) valheim_update_check ; menu ;;
+		    0) exit 0 ;;
+		    *) echo -e $red"Wrong option."$clear; WrongCommand;;
+        esac
+}
+
+# Call the menu function
+menu
