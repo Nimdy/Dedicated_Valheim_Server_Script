26b56fa849dc5624661a27560d17f2580bec06d5
Zerobandwidth <nimdavirus@gmail.com>
2021-02-14 17:48:38 +0100

    Update build_dedicated_valheim_server.sh

--- build_dedicated_valheim_server.sh
+++ build_dedicated_valheim_server.sh
@@ -49,7 +49,7 @@ sleep 1
 
 #EDIT HERE #1
 #build account to run Valheim
-tput setaf 1; echo "Building steam account NON-ROOT"
+tput setaf 1; echo "Building steam account NONROOT"
 sleep 1
 useradd --create-home --shell /bin/bash --password $userpassword steam
 cp /etc/skel/.bashrc /home/steam/.bashrc
