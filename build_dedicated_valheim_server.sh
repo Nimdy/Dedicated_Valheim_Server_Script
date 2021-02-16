f718f805aa062952201090de1b488f9cd3e48c97
Zerobandwidth <nimdavirus@gmail.com>
2021-02-16 08:52:09 +0100

    Update build_dedicated_valheim_server.sh

--- build_dedicated_valheim_server.sh
+++ build_dedicated_valheim_server.sh
@@ -7,10 +7,10 @@
 
 # There are 4 things you need to change!
 # NOTE: Minimum password length is 5 characters & Password cant be in the server name.
-userpassword="user_password"
-password="passw0rd"
-displayname="server display name"
-worldname="111111111"
+userpassword='"user_password"'
+password='"passw0rd"'
+displayname='"server display name"'
+worldname='"111111111"'
 
 #check for updates and upgrade the system auto yes
 tput setaf 2; echo "Checking for upgrades"
@@ -49,7 +49,8 @@ sleep 1
 
 #EDIT HERE #1
 #build account to run Valheim
-tput setaf 1; echo "Building steam account NON-ROOT"
+tput setaf 1; echo "Building steam account NONROOT"
+sleep 1
 useradd --create-home --shell /bin/bash --password $userpassword steam
 cp /etc/skel/.bashrc /home/steam/.bashrc
 cp /etc/skel/.profile /home/steam/.profile
@@ -81,8 +82,8 @@ tput setaf 9;
 sleep 1
 
 #build config for start_valheim.sh
-tput setaf 1; echo "Building Valheim start_valheim server configuration"
 tput setaf 1; echo "Deleting old configuration if file exist"
+tput setaf 1; echo "Building Valheim start_valheim server configuration"
 rm /home/steam/valheimserver/start_valheim.sh
 sleep 1
 cat >> /home/steam/valheimserver/start_valheim.sh <<EOF
@@ -102,8 +103,8 @@ tput setaf 9;
 sleep 1
 
 #build check log script
-tput setaf 1; echo "Building check log script"
 tput setaf 1; echo "Deleting old configuration if file exist"
+tput setaf 1; echo "Building check log script"
 rm /home/steam/check_log.sh
 sleep 1
 cat >> /home/steam/check_log.sh <<EOF
@@ -124,8 +125,8 @@ tput setaf 9;
 sleep 1
 
 #build systemctl configurations for execution of processes for Valheim Server
-tput setaf 1; echo "Building systemctl instructions for Valheim"
 tput setaf 1; echo "Deleting old configuration if file exist"
+tput setaf 1; echo "Building systemctl instructions for Valheim"
 rm /etc/systemd/system/valheimserver.service
 sleep 1
 cat >> /etc/systemd/system/valheimserver.service <<EOF
