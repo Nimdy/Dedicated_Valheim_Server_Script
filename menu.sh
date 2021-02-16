92678349ee210951376f41d3624f3e19f28c7f10
Zerobandwidth <nimdavirus@gmail.com>
2021-02-16 15:53:52 +0100

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -29,7 +29,7 @@ clear
 
 
 function script_check_update() {
-BRANCH="https://github.com/Nimdy/Dedicated_Valheim_Server_Script.git"
+BRANCH="https://github.com/Nimdy/Dedicated_Valheim_Server_Script/tree/beta"
     git remote update
     LAST_UPDATE=`git show --no-notes --format=format:"%H" $BRANCH | head -n 1`
     LAST_COMMIT=`git show --no-notes --format=format:"%H" origin/$BRANCH | head -n 1`
