e6f2b2dc6501476fc55fe67c380f346ca3c8ca71
Zerobandwidth <nimdavirus@gmail.com>
2021-02-16 13:05:47 +0100

    Create roytechsupport.sh
    file used to help others with tech support

---
+++ roytechsupport.sh
@@ -0,0 +1,43 @@
+tech_support() {
+echo ""
+    echo -e "-------------------------------System Information----------------------------"
+    echo -e "Hostname:\t\t"`hostname`
+    echo -e "uptime:\t\t\t"`uptime | awk '{print $3,$4}' | sed 's/,//'`
+    echo -e "Manufacturer:\t\t"`cat /sys/class/dmi/id/chassis_vendor`
+    echo -e "Product Name:\t\t"`cat /sys/class/dmi/id/product_name`
+    echo -e "Version:\t\t"`cat /sys/class/dmi/id/product_version`
+    echo -e "Serial Number:\t\t"`cat /sys/class/dmi/id/product_serial`
+    echo -e "Machine Type:\t\t"`vserver=$(lscpu | grep Hypervisor | wc -l); if [ $vserver -gt 0 ]; then echo "VM"; else echo "Physical"; fi`
+    echo -e "Operating System:\t"`hostnamectl | grep "Operating System" | cut -d ' ' -f5-`
+    echo -e "Kernel:\t\t\t"`uname -r`
+    echo -e "Architecture:\t\t"`arch`
+    echo -e "Processor Name:\t\t"`awk -F':' '/^model name/ {print $2}' /proc/cpuinfo | uniq | sed -e 's/^[ \t]*//'`
+    echo -e "Active User:\t\t"`w | cut -d ' ' -f1 | grep -v USER | xargs -n1`
+    echo -e "System Main IP:\t\t"`hostname -I`
+echo ""
+echo ""
+    echo -e "-------------------------------CPU/Memory Usage------------------------------"
+    echo -e "Memory Usage:\t"`free | awk '/Mem/{printf("%.2f%"), $3/$2*100}'`
+    echo -e "Swap Usage:\t"`free | awk '/Swap/{printf("%.2f%"), $3/$2*100}'`
+    echo -e "CPU Usage:\t"`cat /proc/stat | awk '/cpu/{printf("%.2f%\n"), ($2+$4)*100/($2+$4+$5)}' |  awk '{print $0}' | head -1`
+echo ""
+echo ""
+    echo -e "-------------------------------Disk Usage >80%-------------------------------"
+    df -Ph | sed s/%//g | awk '{ if($5 > 80) print $0;}'
+echo ""
+echo ""
+    echo -e "-------------------------------Valheim Config--------------------------------"
+    cat /home/steam/valheimserver/start_valheim.sh
+echo ""
+echo ""
+    echo -e "-------------------------------Port Information------------------------------"
+sudo ss -tulpn
+echo ""
+echo ""
+    echo -e "-------------------------------SYSLOG INFO-----------------------------------"
+     cat /var/log/syslog
+echo "" 
+
+}
+
+tech_support
