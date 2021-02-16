713b3716997c19819e8b3ae6bd27afe0270a3fa4
Zerobandwidth <nimdavirus@gmail.com>
2021-02-16 09:44:56 +0100

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -29,12 +29,16 @@ clear
 
 
 function script_check_update() {
-    echo ""
-	echo "Add script update check "
-	echo "check if Master Repo is updated"
-	echo " if true download and replace"
-	echo " if not print already up todate nothing to do"
-    echo ""
+BRANCH="https://github.com/Nimdy/Dedicated_Valheim_Server_Script.git"
+    git remote update
+    LAST_UPDATE=`git show --no-notes --format=format:"%H" $BRANCH | head -n 1`
+    LAST_COMMIT=`git show --no-notes --format=format:"%H" origin/$BRANCH | head -n 1`
+        if [ $LAST_COMMIT != $LAST_UPDATE ]; then
+            echo "Updating your branch $BRANCH"
+            git pull --no-edit
+        else
+            echo "No updates available"
+        fi
 }
 
 function memory_check() {
