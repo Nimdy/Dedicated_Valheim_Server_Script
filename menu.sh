7d7c412c0e2fa4fc7e7a18971e656f13c08c670b
Zerobandwidth <nimdavirus@gmail.com>
2021-02-17 13:26:24 +0100

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -25,6 +25,57 @@ clear
 
 ##
 
+##
+# Color  Variables
+##
+
+NOCOLOR='\033[0m'
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHTRED='\033[1;31m'
+LIGHTGREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+WHITE='\033[1;37m'
+CLEAR='\e[0m'
+
+
+##
+# Color Functions
+##
+ColorRed(){
+	echo -ne $RED$1$CLEAR
+}
+ColorGreen(){
+	echo -ne $GREEN$1$CLEAR
+}
+ColorOrange(){
+	echo -ne $ORANGE$1$CLEAR
+}
+ColorBlue(){
+	echo -ne $BLUE$1$CLEAR
+}
+ColorPurple(){
+	echo -ne $PURPLE$1$CLEAR
+}
+ColorCyan(){
+	echo -ne $CYAN$1$CLEAR
+}
+ColorLightRed(){
+	echo -ne $LIGHTRED$1$CLEAR
+}
+ColorLightGreen(){
+	echo -ne $LIGHTGREEN$1$CLEAR
+}
+ColorYellow(){
+	echo -ne $LIGHTYELLOW$1$CLEAR
+}
+ColorWhite(){
+	echo -ne $WHITE$1$CLEAR
+}
 
 
 
@@ -404,58 +455,6 @@ function apply_server_updates() {
 
 }
 
-
-##
-# Color  Variables
-##
-
-NOCOLOR='\033[0m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHTRED='\033[1;31m'
-LIGHTGREEN='\033[1;32m'
-YELLOW='\033[1;33m'
-WHITE='\033[1;37m'
-CLEAR='\e[0m'
-
-
-##
-# Color Functions
-##
-ColorRed(){
-	echo -ne $RED$1$CLEAR
-}
-ColorGreen(){
-	echo -ne $GREEN$1$CLEAR
-}
-ColorOrange(){
-	echo -ne $ORANGE$1$CLEAR
-}
-ColorBlue(){
-	echo -ne $BLUE$1$CLEAR
-}
-ColorPurple(){
-	echo -ne $PURPLE$1$CLEAR
-}
-ColorCyan(){
-	echo -ne $CYAN$1$CLEAR
-}
-ColorLightRed(){
-	echo -ne $LIGHTRED$1$CLEAR
-}
-ColorLightGreen(){
-	echo -ne $LIGHTGREEN$1$CLEAR
-}
-ColorYellow(){
-	echo -ne $LIGHTYELLOW$1$CLEAR
-}
-ColorWhite(){
-	echo -ne $WHITE$1$CLEAR
-}
 
 server_install_menu(){
 echo -ne "
