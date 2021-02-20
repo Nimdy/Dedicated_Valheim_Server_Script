7b03424625b2505507510e7dfb0de1e335ff6b04
Zerobandwidth <nimdavirus@gmail.com>
2021-02-20 08:07:17 +0100

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -245,9 +245,25 @@ sleep 1
     tput setaf 2; echo "I swear to LOKI, you better NOT use Special Characters" ; tput setaf 9; 
 done
 echo ""
+cat >> /home/steam/serverSetup.txt <<EOF
+echo "---------------------------------------------------------------"
+echo "Here is the information you entered"
+echo "This information is for you to ref later, in case you forgot"
+tput setaf 2; echo "---------------------------------------" ; tput setaf 9;
+tput setaf 2; echo "nonroot steam password:  $userpassword " ; tput setaf 9;
+tput setaf 2; echo "Public Server Name:      $displayname " ; tput setaf 9;
+tput setaf 2; echo "Local World Name:        $worldname " ; tput setaf 9;
+tput setaf 2; echo "Valheim Server Password: $password " ; tput setaf 9;
+tput setaf 2; echo "---------------------------------------" ; tput setaf 9;
+echo ""
+echo "Each time this is ran, the past info will be added to each line"
+echo "---------------------------------------------------------------"
+EOF
+chown steam:steam /home/steam/serverSetup.txt
 clear
 echo "Here is the information you entered"
-echo "This information is only saved in the valheim_server.sh file"
+echo "This information is saved in the valheim_server.sh file"
+echo "This information is saved in /home/steam/serverSetup.txt for referance later, if you forget"
 tput setaf 2; echo "---------------------------------------" ; tput setaf 9;
 tput setaf 2; echo "nonroot steam password:  $userpassword " ; tput setaf 9;
 tput setaf 2; echo "Public Server Name:      $displayname " ; tput setaf 9;
