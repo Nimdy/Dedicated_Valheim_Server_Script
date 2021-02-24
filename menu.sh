5a95589892e0589fbcccc2c1a97b6fac3735640a
Zerobandwidth <nimdavirus@gmail.com>
2021-02-24 11:11:25 +0100

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -110,7 +110,7 @@ ColorWhite(){
 ########################################################################
 
 function script_check_update() {
-BRANCH="beta"
+BRANCH="https://github.com/Nimdy/Dedicated_Valheim_Server_Script.git"
     git stash apply
     LAST_UPDATE=`git show --no-notes --format=format:"%H" $BRANCH | head -n 1`
     LAST_COMMIT=`git show --no-notes --format=format:"%H" origin/$BRANCH | head -n 1`
