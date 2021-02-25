7ebb87a30593c04bf64cc37d0407e87a19b45a56
Zerobandwidth <nimdavirus@gmail.com>
2021-02-25 16:42:05 +0100

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -29,7 +29,7 @@ backupPath=/home/steam/backups
 ###############################################################
 
 # Set Menu Version
-mversion="Version 1.7.1"
+mversion="Version 1.7.2"
 ##
 # Update Menu script 
 ##
