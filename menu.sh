94430828fc0171662599042e3a46493420514d38
Zerobandwidth <nimdavirus@gmail.com>
2021-03-05 08:29:56 +0100

    ver update

--- menu.sh
+++ menu.sh
@@ -29,7 +29,7 @@ backupPath=/home/steam/backups
 ###############################################################
 
 # Set Menu Version for menu display
-mversion="1.8.1-Loki"
+mversion="1.9-Fenrir"
 
 ##
 # Update Menu script 
