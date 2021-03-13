ce460f92eb92d8393abe20e52980a9bc8bde2046
Zerobandwidth <nimdavirus@gmail.com>
2021-03-13 17:07:17 +0100

    Update advancemenu.sh

--- advancemenu.sh
+++ advancemenu.sh
@@ -32,7 +32,7 @@ backupPath=/home/steam/backups
 export valheimVanilla=1
 
 # Set Menu Version for menu display
-mversion="2.0.3-Lofn"
+mversion="2.1-Lofn"
 
 ##
 # Our WIKI has pretty much answered every question and if not we will add yours
