ea78498440aa7616dcd30296eccbb64001a3c955
Zerobandwidth <nimdavirus@gmail.com>
2021-03-31 07:12:12 +0200

    updated valheim check

--- menu.sh
+++ menu.sh
@@ -40,7 +40,7 @@ backupPath=/home/steam/backups
 ###############################################################
 
 # Set Menu Version for menu display
-mversion="2.3-Lofn"
+mversion="2.3.1-Lofn"
 
 
 ########################################################################
