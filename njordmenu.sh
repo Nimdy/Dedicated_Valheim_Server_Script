e81957eb4b54ce9c7bfd5d0b5b31e781555d6790
Zerobandwidth <nimdavirus@gmail.com>
2021-05-25 17:25:51 +0200

    Update njordmenu.sh

--- njordmenu.sh
+++ njordmenu.sh
@@ -3018,7 +3018,7 @@ $(ColorOrange '║')" $(display_public_IP)
 	echo -ne "
 $(ColorOrange '║')" $(display_local_IP) 
 	echo -ne "
-$(ColorOrange '║') $FUNCTION_HEADER_MENU_INFO_SERVER_PORT " ${currentPort}
+$(ColorOrange '║') $FUNCTION_HEADER_MENU_INFO_SERVER_PORT " 	echo -ne " $(ColorGreen ''" ${currentPort}"'')"
 	echo -ne "
 $(ColorOrange '║') $FUNCTION_HEADER_MENU_INFO_SERVER_AT_GLANCE" $(server_status)
 	echo -ne " 
