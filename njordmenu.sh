ba34e33fa5457e7632bf4e991070065ef32a6fad
Zerobandwidth <nimdavirus@gmail.com>
2021-05-25 17:16:52 +0200

    Update njordmenu.sh

--- njordmenu.sh
+++ njordmenu.sh
@@ -3006,10 +3006,10 @@ echo -ne "
 $(ColorOrange '╔══════════════════════════════════════════════════════════╗')
 $(ColorOrange '║~~~~~~~~~~*****~~~~~~~~-Njord Menu-~~~~~~~~~*****~~~~~~~~~║')
 $(ColorOrange '╠══════════════════════════════════════════════════════════╝')
-$(ColorOrange '║'"$FUNCTION_HEADER_MENU_INFO_VALHEIM_OFFICIAL_BUILD"'')" $(check_official_valheim_release_build)
+$(ColorOrange '║'" $FUNCTION_HEADER_MENU_INFO_VALHEIM_OFFICIAL_BUILD"'')" $(check_official_valheim_release_build)
 	echo -ne "
-$(ColorOrange '║'"$FUNCTION_HEADER_MENU_INFO_VALHEIM_LOCAL_BUILD"' ') $(check_local_valheim_build)
-$(ColorOrange '║')"$FUNCTION_HEADER_MENU_INFO_LD_SEVER_SESSION ${worldname}
+$(ColorOrange '║'" $FUNCTION_HEADER_MENU_INFO_VALHEIM_LOCAL_BUILD"' ') $(check_local_valheim_build)
+$(ColorOrange '║')" $FUNCTION_HEADER_MENU_INFO_LD_SEVER_SESSION ${worldname}
 	echo -ne "
 $(ColorOrange '║')" $FUNCTION_HEADER_MENU_INFO_SERVER_NAME ${currentDisplayName}
 	echo -ne " 
