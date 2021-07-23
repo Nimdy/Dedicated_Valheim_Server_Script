56418b1913721ee3b9c3da69a2b83f2094f63577
Zerobandwidth <nimdavirus@gmail.com>
2021-07-23 09:17:12 +0200

    trying stuff

---
+++ gui/commands.sh
@@ -0,0 +1,12 @@
+#1/bin/bash
+
+#trying some different things here to limit www-data and php access but giving it enough power to function correctly
+
+
+$valheimStart = sudo systemctl start valheimserver_*
+$valheimStop = sudo systemctl stop valheimserver_*
+$valheimRestart = sudo systemctl restart valheimserver_*
+$valheimStatus = sudo systemctl status valheimserver_*
+
+$valheimCopy = sudo cp -R /home/steam/*
+$valheimSteamPermissions = sudo chown -Rf steam:steam /home/steam/*
