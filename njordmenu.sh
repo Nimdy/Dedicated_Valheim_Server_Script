099c07ac3ef113739b5722c06252fedd89f7f7ec
Zerobandwidth <nimdavirus@gmail.com>
2023-04-23 18:36:15 -0600

    Update njordmenu.sh

--- njordmenu.sh
+++ njordmenu.sh
@@ -96,7 +96,7 @@ debugmsg="n"
 # if [ "$debugmsg" == "y" ] ; then echo "something" ; fi
 ###############################################################
 # Set Menu Version for menu display
-mversion="3.2.1-Lofns-Mist"
+mversion="3.5-lokis-dance"
 ldversion="0.4.051120211500ET.dev"
 ###      -- Use are your own risk -- 
 ### dev   -- Still working on firewall code. 
